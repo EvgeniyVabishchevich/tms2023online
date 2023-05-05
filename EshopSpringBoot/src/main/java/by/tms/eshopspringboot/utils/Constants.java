3280449ce763410702d5ec53b28fc5baaadaf3e3
Evgeniy Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2023-05-05 11:38:00 +0300

    Added UtiliryClass annotation to Class Constants and its inner classes

--- EshopSpringBoot/src/main/java/by/tms/eshopspringboot/utils/Constants.java
+++ EshopSpringBoot/src/main/java/by/tms/eshopspringboot/utils/Constants.java
@@ -1,48 +1,51 @@
 package by.tms.eshopspringboot.utils;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public final class Constants {
-    private Constants(){}
 
+    @UtilityClass
     public class MappingPath {
-        private MappingPath(){}
-        public static final String ADMIN = "admin/admin";
-        public static final String BUY = "buy";
-        public static final String CART = "cart";
-        public static final String CATEGORIES = "categories";
-        public static final String ERROR404 = "error404";
-        public static final String LOGIN = "login";
-        public static final String REGISTER = "register";
-        public static final String PRODUCTS = "products";
-        public static final String USER = "user";
-        public static final String SEARCH_RESULT = "searchResult";
-        public static final String SEARCH = "search";
+        public final String ADMIN = "admin/admin";
+        public final String BUY = "buy";
+        public final String CART = "cart";
+        public final String CATEGORIES = "categories";
+        public final String ERROR404 = "error404";
+        public final String LOGIN = "login";
+        public final String REGISTER = "register";
+        public final String PRODUCTS = "products";
+        public final String USER = "user";
+        public final String SEARCH_RESULT = "searchResult";
+        public final String SEARCH = "search";
     }
 
+    @UtilityClass
     public class Attributes {
-        private Attributes(){}
-        public static final String PRODUCTS_MAP = "productsMap";
-        public static final String TOTAL_PRICE = "totalPrice";
-        public static final String CATEGORIES = "categories";
-        public static final String PRODUCTS = "products";
-        public static final String SELECTED_CATEGORY = "selectedCategory";
-        public static final String SEARCH_REQUEST = "searchRequest";
-        public static final String MIN_PRICE = "minPrice";
-        public static final String MAX_PRICE = "maxPrice";
+        public final String PRODUCTS_MAP = "productsMap";
+        public final String TOTAL_PRICE = "totalPrice";
+        public final String CATEGORIES = "categories";
+        public final String PRODUCTS = "products";
+        public final String SELECTED_CATEGORY = "selectedCategory";
+        public final String SEARCH_REQUEST = "searchRequest";
+        public final String MIN_PRICE = "minPrice";
+        public final String MAX_PRICE = "maxPrice";
     }
+
+    @UtilityClass
     public class RequestParameters {
-        private RequestParameters(){}
-        public static final String PASSWORD_REPEAT = "passwordRepeat";
-        public static final String PRODUCT_ID = "productId";
-        public static final String CATEGORIES = "categories";
-        public static final String IMAGE = "image";
-        public static final String DESCRIPTION = "description";
-        public static final String PRICE = "price";
-        public static final String NAME = "name";
-        public static final String SEARCH_REQUEST = "searchRequest";
-        public static final String MIN_PRICE = "minPrice";
-        public static final String MAX_PRICE = "maxPrice";
-        public static final String CATEGORY = "category";
-        public static final String SELECTED_CATEGORY = "selectedCategory";
-        public static final String PRODUCTS = "products";
+        public final String PASSWORD_REPEAT = "passwordRepeat";
+        public final String PRODUCT_ID = "productId";
+        public final String CATEGORIES = "categories";
+        public final String IMAGE = "image";
+        public final String DESCRIPTION = "description";
+        public final String PRICE = "price";
+        public final String NAME = "name";
+        public final String SEARCH_REQUEST = "searchRequest";
+        public final String MIN_PRICE = "minPrice";
+        public final String MAX_PRICE = "maxPrice";
+        public final String CATEGORY = "category";
+        public final String SELECTED_CATEGORY = "selectedCategory";
+        public final String PRODUCTS = "products";
     }
 }
