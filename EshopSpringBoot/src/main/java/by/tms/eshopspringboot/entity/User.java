5279aa3c174632f15050e03072ca57260fff864a
Evgeniy Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2023-05-06 14:21:55 +0300

    .

--- EshopSpringBoot/src/main/java/by/tms/eshopspringboot/entity/User.java
+++ EshopSpringBoot/src/main/java/by/tms/eshopspringboot/entity/User.java
@@ -33,7 +33,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "user_type", columnDefinition = "privilege")
+    @Column(name = "user_type")
     @Enumerated(EnumType.STRING)
     @Type(PostgreSQLEnumType.class)
     private UserType userType;
