0ba14b6fefa3a424801abafe51e8b3b3b32c6562
Evgeniy Vabishchevich <evgeniy.vabishevich.99@mail.ru>
2023-06-25 11:06:58 +0300

    Moved @Autowired from setter to constructor in UserDetailsService class

--- EshopSpringBoot/src/main/java/by/tms/eshopspringboot/security/UserDetailsService.java
+++ EshopSpringBoot/src/main/java/by/tms/eshopspringboot/security/UserDetailsService.java
@@ -2,22 +2,19 @@ package by.tms.eshopspringboot.security;
 
 import by.tms.eshopspringboot.entity.User;
 import by.tms.eshopspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException(username));
         return new UserDetails(user);
-    }
-
-    @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
     }
 }
